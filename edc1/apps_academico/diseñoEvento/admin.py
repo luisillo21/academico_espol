08641835c589cfd2f36ec70f85164cdb7f06c9c4
luisillo21 <luisardilamacias@gmail.com>
2020-03-02 11:10:09 -0500

    cambios en admin

--- edc1/apps_academico/diseñoEvento/admin.py
+++ edc1/apps_academico/diseñoEvento/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
 
+admin.site.register(DesignEvento)
+admin.site.register(Especialidad)
+admin.site.register(Area)
+admin.site.register(TipoEvento)
 
 
 
